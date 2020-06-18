ae07d0290df8f4aa6f10980e9c198fe46c42da95
greg <greg.sypek@gmail.com>
2020-06-18 21:57:50 +0200

    add picture media in HTML and fix problems with displaying 'bigImage'

--- js/gallery.js
+++ js/gallery.js
@@ -1,27 +1,36 @@
 const gallery = [...document.querySelectorAll('.gallery_icons div a img')];
-const bigImg = document.querySelector('.gallery_bigImage img');
+let bigImg = document.querySelector('.gallery_bigImage img');
 
 
-const changeImage =(e)=> {
+const handleClick =(e)=> {
   e.preventDefault();
   
-  const selected = e.target.dataset.pic;
-
-  // index was taken from dataset attribut as a string and converted into number which reflect proper index in gallery array.
-
+  
+  let selected = e.target.dataset.pic;
   let index = selected.slice(3) - 1;
-  bigImg.src = `./img/${selected}.jpg`;
+  
+    if(window.innerWidth > 768) {
+  
+    // index was taken from dataset attribut as a string and converted into number which reflect proper index in gallery array.
+  
+    bigImg.src = `./img/${selected}.jpg`;
 
+    // change default source picture in DOM
+    let x = document.getElementById('default');
+    x.srcset = `./img/${selected}.jpg`;
 
+  } else {
+  bigImg.src = `./img/${selected}_small.jpg`;
+  
+}    
 const activeDot = gallery.findIndex(img=>img.classList.contains('active-icon'));
-    gallery[activeDot].classList.remove('active-icon');
-    gallery[index].classList.add('active-icon');
-    
-    
+gallery[activeDot].classList.remove('active-icon');
+gallery[index].classList.add('active-icon');
 
 }
-gallery.forEach(item=>item.addEventListener('click', changeImage));
 
+
+gallery.forEach(item=>item.addEventListener('click', handleClick));
 
 
 
